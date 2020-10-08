cbefa8b7f5366f81a8ac7b3c2918a6dda4da922f
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-08 18:19:04 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_concat(array,array_2)
+  new_array = array.concat(array_2)
+  return new_array 
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  new_array = array.flatten()
+  return new_array
+end
+
+def using_insert(array, element)
+  new_array = []
+  new_array[4] = array.insert(element)
+  return new_array 
+end 
